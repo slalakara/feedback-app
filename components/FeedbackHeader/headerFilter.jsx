429e661d8f2bfab033d9bb76f7b68b7c0cb59800
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-26 17:34:19 +0300

    filter options eklendi

--- components/FeedbackHeader/headerFilter.jsx
+++ components/FeedbackHeader/headerFilter.jsx
@@ -1,8 +1,27 @@
 import "./feedbackHeader.css"
 
-export default function HeaderFilter(){
-  return(
+export default function HeaderFilter() {
+  return (
     <>
+      <div className="filter-options">
+        <p>
+          <input
+            type="checkbox" />
+          Most Upvotes
+        </p>
+        <p>
+          <input type="checkbox" />
+          Least Upvotes
+        </p>
+        <p>
+          <input type="checkbox" />
+          Most Comments
+        </p>
+        <p>
+          <input type="checkbox" />
+          Least Comments
+        </p>
+      </div>
     </>
   )
 }

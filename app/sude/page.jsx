7b4a8019936b61ecccea989e34b118870c44fe8b
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-27 00:02:24 +0300

    edit page css düzenlemesi

--- app/sude/page.jsx
+++ app/sude/page.jsx
@@ -3,13 +3,14 @@ import FeedbackHeader from "@/components/FeedbackHeader/feedbackHeader";
 import HeaderFilter from "@/components/FeedbackHeader/headerFilter";
 import "./sude.css"
 import NewPage from "@/components/new/page";
+import EditModal from "@/components/EditFeedbackComp/page";
 export default function deneme() {
   return (
     <>
       <div className="deniyorum">
         <FeedbackHeader />
         <HeaderFilter />
-        <NewPage/>
+        <EditModal/>
         <AddFeedbackBtn />
 
       </div>

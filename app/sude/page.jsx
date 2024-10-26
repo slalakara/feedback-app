6c707fed61c66500d3d0199d212fd8bc1c4e775f
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-26 23:52:34 +0300

    yeni ekleme sayfası ve cssi düzenlendi

--- app/sude/page.jsx
+++ app/sude/page.jsx
@@ -2,13 +2,16 @@ import AddFeedbackBtn from "@/components/FeedbackHeader/addFeedbackBtn";
 import FeedbackHeader from "@/components/FeedbackHeader/feedbackHeader";
 import HeaderFilter from "@/components/FeedbackHeader/headerFilter";
 import "./sude.css"
+import NewPage from "@/components/new/page";
 export default function deneme() {
   return (
     <>
       <div className="deniyorum">
         <FeedbackHeader />
         <HeaderFilter />
+        <NewPage/>
         <AddFeedbackBtn />
+
       </div>
 
     </>
---
+++ components/new/page.jsx
@@ -0,0 +1,56 @@
+import "./new.css";
+import AddLogo from "../svgs/add";
+import GoBack from "../GoBack/gobackBtn";
+
+export default function NewPage() {
+  return (
+    <div className="newModalContainer">
+      <div className="gobackbtn">
+        <GoBack />
+      </div>
+      <div className="newModal">
+        <div className="addlogosvg">
+          <AddLogo />
+        </div>
+        <div className="newHero">
+          <h1>Create New Feedback</h1>
+
+          <div className="feedback">
+            <div className="feedbackTitle">
+              <h3>Feedback Title</h3>
+              <p>Add a short, descriptive headline</p>
+            </div>
+            <input type="text" />
+          </div>
+
+          <div className="category">
+            <div className="categoryTitle">
+              <h3>Category</h3>
+              <p>Choose a category for your feedback</p>
+            </div>
+            <select>
+              <option value="Feature">Feature</option>
+              <option value="UI">UI</option>
+              <option value="UX">UX</option>
+              <option value="Enhancement">Enhancement</option>
+              <option value="Bug">Bug</option>
+            </select>
+          </div>
+
+          <div className="details">
+            <div className="detailsTitle">
+              <h3>Feedback Detail</h3>
+              <p>Include any specific comments on what should be improved, added, etc.</p>
+            </div>
+            <textarea></textarea>
+          </div>
+
+          <div className="buttons">
+            <button className="cancel">Cancel</button>
+            <button className="addFeedback">Add Feedback</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

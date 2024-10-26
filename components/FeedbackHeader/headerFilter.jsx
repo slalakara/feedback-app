32ee541963f171a1d93db2ea39236721f52c7f55
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-26 22:11:09 +0300

    svg componente eklendi

--- components/FeedbackHeader/headerFilter.jsx
+++ components/FeedbackHeader/headerFilter.jsx
@@ -1,10 +1,13 @@
 "use client";
 import { useState } from "react";
+import OpenFilter from "../svgs/openfilter";
+import DefaultFilter from "../svgs/defaultfilter";
 import "./feedbackHeader.css";
 
 export default function HeaderFilter() {
   const [selectedFilter, setSelectedFilter] = useState("Most Upvotes");
   const [showOptions, setShowOptions] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   const handleFilterClick = (filter) => {
     setSelectedFilter(filter);
@@ -15,7 +18,7 @@ export default function HeaderFilter() {
   };
 
   const handleMouseLeave = () => {
-    setTimeout(() => setShowOptions(false), 10000);
+    setShowOptions(false);
   };
 
   return (
@@ -25,53 +28,44 @@ export default function HeaderFilter() {
       onMouseLeave={handleMouseLeave}
     >
       <p className="filter-text">
-        Sort by: <strong>{selectedFilter}</strong> 
+        Sort by : <strong>{selectedFilter}</strong>
+        <span
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          style={{ marginLeft: "8px", display: "inline-flex", alignItems: "center" }}
+        >
+          {isHovered ? <DefaultFilter /> : <OpenFilter />}
+        </span>
       </p>
       {showOptions && (
         <div className="filter-options">
           <p
-            className={`filter-option ${
-              selectedFilter === "Most Upvotes" ? "selected" : ""
-            }`}
+            className={`filter-option ${selectedFilter === "Most Upvotes" ? "selected" : ""
+              }`}
             onClick={() => handleFilterClick("Most Upvotes")}
           >
-            Most Upvotes{" "}
-            {selectedFilter === "Most Upvotes" && (
-              <span className="check-mark">✔</span>
-            )}
+            Most Upvotes {selectedFilter === "Most Upvotes" && <span className="check-mark">✔</span>}
           </p>
           <p
-            className={`filter-option ${
-              selectedFilter === "Least Upvotes" ? "selected" : ""
-            }`}
+            className={`filter-option ${selectedFilter === "Least Upvotes" ? "selected" : ""
+              }`}
             onClick={() => handleFilterClick("Least Upvotes")}
           >
-            Least Upvotes{" "}
-            {selectedFilter === "Least Upvotes" && (
-              <span className="check-mark">✔</span>
-            )}
+            Least Upvotes {selectedFilter === "Least Upvotes" && <span className="check-mark">✔</span>}
           </p>
           <p
-            className={`filter-option ${
-              selectedFilter === "Most Comments" ? "selected" : ""
-            }`}
+            className={`filter-option ${selectedFilter === "Most Comments" ? "selected" : ""
+              }`}
             onClick={() => handleFilterClick("Most Comments")}
           >
-            Most Comments{" "}
-            {selectedFilter === "Most Comments" && (
-              <span className="check-mark">✔</span>
-            )}
+            Most Comments {selectedFilter === "Most Comments" && <span className="check-mark">✔</span>}
           </p>
           <p
-            className={`filter-option ${
-              selectedFilter === "Least Comments" ? "selected" : ""
-            }`}
+            className={`filter-option ${selectedFilter === "Least Comments" ? "selected" : ""
+              }`}
             onClick={() => handleFilterClick("Least Comments")}
           >
-            Least Comments{" "}
-            {selectedFilter === "Least Comments" && (
-              <span className="check-mark">✔</span>
-            )}
+            Least Comments {selectedFilter === "Least Comments" && <span className="check-mark">✔</span>}
           </p>
         </div>
       )}

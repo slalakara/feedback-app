efe16bea610448ccf478910189ce709eb5c2c4c9
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-26 16:43:59 +0300

    add feedback button oluşturuldu

--- components/FeedbackHeader/addFeedbackBtn.jsx
+++ components/FeedbackHeader/addFeedbackBtn.jsx
@@ -3,6 +3,9 @@ import "./feedbackHeader.css"
 export default function AddFeedbackBtn(){
   return(
     <>
+    <div className="addfeedback">
+      <button>+ Add Feedback</button>
+    </div>
     </>
   )
 }

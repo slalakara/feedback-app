781219b7a42c99ccc0a0eccd33182e1dbb36d1e7
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-28 22:06:26 +0300

    yourm ekleme eklendi

--- components/addComments/addComments.jsx
+++ components/addComments/addComments.jsx
@@ -1,35 +1,43 @@
-"use client";
-import "./addComments.css";
-import { useEffect, useState } from "react";
-import { toast } from "sonner";
+"use client"; 
+import "./addComments.css"; 
+import { useEffect, useState } from "react"; 
+import { toast } from "sonner"; 
 
-export default function AddComment() {
-  const [text, setText] = useState("");
-  const MAX_CHAR = 250;
-  const [remainigChar, setRemaningChar] = useState(250);
+export default function AddComment({ onCommentAdded }) {
+  const [text, setText] = useState(""); 
+  const MAX_CHAR = 250; 
+  const [remainingChar, setRemainingChar] = useState(MAX_CHAR); 
 
-  useEffect(() => {
-    setRemaningChar(MAX_CHAR - text.length);
+  useEffect(() => { 
+    setRemainingChar(MAX_CHAR - text.length); 
 
-    if (remainigChar <= 0) {
-      toast.error("250 karaktaerden fazla");
-    }
-  }, [text]);
+    if (remainingChar < 0) { 
+      toast.error("250 karakterden fazla olamaz"); 
+    } 
+  }, [text]); 
 
-  return (
-    <div className="addCommentContainer">
-      <h4>Add Comment</h4>
-      <form>
-        <textarea
-          onChange={(e) => setText(e.target.value)}
-          value={text}
-          placeholder="type your comment here"
-        ></textarea>
-        <div className="commentFooter">
-          <span>{remainigChar} Characters left</span>
-          <button className="postcommentbtn" disabled={remainigChar <= 0}>Post Comment</button>
-        </div>
-      </form>
-    </div>
-  );
+  const handleSubmit = (e) => { 
+    e.preventDefault();
+    if (text.length > 0 && remainingChar >= 0) { 
+      onCommentAdded(text); 
+      setText(""); 
+    } 
+  }; 
+
+  return ( 
+    <div className="addCommentContainer"> 
+      <h4>Add Comment</h4> 
+      <form onSubmit={handleSubmit}> 
+        <textarea 
+          onChange={(e) => setText(e.target.value)} 
+          value={text} 
+          placeholder="type your comment here" 
+        ></textarea> 
+        <div className="commentFooter"> 
+          <span>{remainingChar} Characters left</span> 
+          <button className="postcommentbtn" disabled={remainingChar < 0}>Post Comment</button> 
+        </div> 
+      </form> 
+    </div> 
+  ); 
 }

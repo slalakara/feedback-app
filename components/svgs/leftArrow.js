5f45252ae8226e13ccba69df941802b7239cf069
Arda Toraman <toramanarda245@gmail.com>
2024-10-29 16:29:41 +0300

    SLALALALAKARA

--- components/svgs/leftArrow.js
+++ components/svgs/leftArrow.js
@@ -1,7 +1,7 @@
 export default function LeftArrow() {
   return (
     <svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M4.33447 9L0.334473 5L4.33447 1" stroke="#4661E6" stroke-width="2" />
+      <path d="M4.33447 9L0.334473 5L4.33447 1" stroke="#4661E6" strokeWidth="2" />
     </svg>
 
   )

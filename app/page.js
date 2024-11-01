203f216290d7bc5763df6e0206e9b74bbc3846e7
Sıla <silakara1453@gmail.com>
2024-11-01 13:17:32 +0300

    bi şeyler değişti

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Page from "./PostDetail/page";
+import LoginPage from "./login/page";
 
 export default function Home() {
   return (
--- utils/service.js
+++ utils/service.js
@@ -16,3 +16,23 @@ export async function fetchFeedbacks(category) {
     return [];
   }
 }
+
+export async function loginFormRequest(url, method= 'POST', data= null, cache = "force-cache") {
+  try{
+    const response = await fetch(url, {
+      method,
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: data ? JSON.stringify(data):null,
+      cache
+    })
+    if(!response.ok){
+      return { response: null, status: response.status, error: response }
+    }
+    const responseData = await response.json();
+    return { response: responseData, status: response.status,error: null }
+  }catch{
+    
+  }
+}

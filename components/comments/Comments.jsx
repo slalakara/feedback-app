e0c832ad2e02db7297a5a5801ff813dab5e0d426
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-11-01 17:04:30 +0300

    detay sayfası ve yorumları çekildi

--- components/comments/Comments.jsx
+++ components/comments/Comments.jsx
@@ -2,27 +2,30 @@ import Elijah from "../imgs/elijah.png";
 import ReplyButton from "./replyBtn"; 
 
 export default function Comments({ comments = [] }) {
-  return ( 
-    <div className="commentsContainer"> 
-      <div className="commentsGeneral"> 
-        <h3>{comments.length} Comments</h3> 
+  if (comments.length === 0) {
+    return <p>Yorum yok.</p>; 
+  }
 
-        {comments.map((comment, index) => ( 
-          <div className="commentsCard" key={index}> 
-            <img src={Elijah} alt="Elijah" /> 
-            <div className="content"> 
-              <div className="userInformation"> 
-                <div> 
-                  <h4>{comment.name || "Name"}</h4> 
-                  <p>@{comment.username || "Username"}</p> 
-                </div> 
-                <ReplyButton /> 
-              </div> 
-              <p>{comment.content || comment}</p> 
-            </div> 
-          </div> 
-        ))} 
-      </div> 
-    </div> 
-  ); 
+  return (
+    <div className="commentsContainer">
+      <div className="commentsGeneral">
+        <h3>{comments.length} Yorum</h3>
+        {comments.map((comment, index) => (
+          <div className="commentsCard" key={index}>
+            <img src={Elijah} alt="Elijah" />
+            <div className="content">
+              <div className="userInformation">
+                <div>
+                  <h4>{comment.name || "Name"}</h4>
+                  <p>@{comment.username || "Username"}</p>
+                </div>
+                <ReplyButton />
+              </div>
+              <p>{comment.content || comment}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
